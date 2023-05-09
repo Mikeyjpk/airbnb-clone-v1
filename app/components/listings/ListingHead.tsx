0d9e37a10de54722a71b960bea7469c4d9179293
Mikeyjpk <michaeljpkerr@gmail.com>
2023-05-09 10:57:41 +1000

    created listing routes

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -2,15 +2,18 @@
 
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
+
 import React from "react";
 import Heading from "../Heading";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingHeadProps {
     title: string;
     locationValue: string;
     imageSrc: string;
     id: string;
-    currentUser?: SafeUser
+    currentUser?: SafeUser | null
 }
 
 const ListingHead: React.FC<ListingHeadProps> = ({
@@ -31,9 +34,26 @@ const ListingHead: React.FC<ListingHeadProps> = ({
             subtitle={`${location?.region}, ${location?.label}`}
             />
             <div
-                
+                className="
+                    w-full
+                    h-[60vh]
+                    overflow-hidden
+                    rounded-xl
+                    relative
+                "
             >
-
+                <Image 
+                    alt="Image"
+                    src={imageSrc}
+                    fill
+                    className="object-cover w-full"
+                />
+                <div className="absolute top-5 right-5">
+                    <HeartButton 
+                        listingId={id}
+                        currentUser={currentUser}
+                    />
+                </div>
             </div>
         </>
     )
--- app/types/index.ts
+++ app/types/index.ts
@@ -5,7 +5,7 @@ export type SafeListing = Omit<
     "createdAt"
 > & {
     createdAt: string;
-}
+};
 
 export type SafeUser = Omit<
     User,
